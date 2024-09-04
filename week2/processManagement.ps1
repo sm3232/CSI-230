82d30fa83240197778fa9fe71ccbc7d0936cf982
sm3232 <connor.hill@mymail.champlain.edu>
2024-09-04 14:02:03 -0400

    processManagement

---
+++ week2/processManagement.ps1
@@ -0,0 +1,19 @@
+﻿# 1
+Get-Process C*
+
+
+# 2
+Get-Process | Where-Object {$_.Path -match "system32"} | Format-Table *
+
+
+# 3
+Get-Service | Where-Object {$_.Status -eq "Stopped"} | Sort-Object -Property DisplayName | Export-Csv -Path "stopped.csv"
+
+
+# 4
+$chromeProcess = Get-Process -Name chrome -ErrorAction SilentlyContinue
+if(-not $chromeProcess){
+    Start-Process chrome -ArgumentList "https://champlain.edu"
+} else {
+    Stop-Process -Name chrome
+}
