f64d31d24e1fee54084851608981f89aad42be72
sm3232 <connor.hill@mymail.champlain.edu>
2024-09-24 12:41:20 -0400

    as2

--- week5/Apache-Logs.ps1
+++ week5/Apache-Logs.ps1
@@ -16,3 +16,22 @@
 
     return ($res | Group-Object -Property IP) | Select-Object Count, Name
 }
+
+function Get-Logs {
+    $logsUnformatted = Get-Content -Path C:\xampp\apache\logs\access.log
+    $records = @()
+    for($i = 0; $i -lt $logsUnformatted.Count; $i++){
+        $words = $logsUnformatted[$i].Split(' ')
+        $records += [pscustomobject]@{"IP"=$words[0]; `
+        "Time" = $words[3].Trim('[').Trim(']'); `
+        "Method" = $words[5].Trim('"'); `
+        "Page" = $words[6]; `
+        "Protocol" = $words[7]; `
+        "Response" = $words[8]; `
+        "Referrer" = $words[2]; `
+        "Client" = $words[11..($words.Count)];}
+    }
+    return $records | Where-Object {$_.IP -ilike "184.*"}
+}
+$records = Get-Logs
+$records | Format-Table -AutoSize -Wrap
